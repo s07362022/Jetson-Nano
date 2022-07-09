6f9994ad569981000ccb212a756d7189df05d0c4
Li_Harden <68153902+s07362022@users.noreply.github.com>
2022-07-09 19:39:03 +0800

    Update sensor_main.py

--- sensor_main.py
+++ sensor_main.py
@@ -3,6 +3,7 @@ import bh1750 as bh1
 import time
 import gps_t
 import influx_test
+import sql_01
 while True:
     temperature, humidity=dht02.dht01()
     time.sleep(2) 
@@ -10,7 +11,11 @@ while True:
     #print(lightLevel)
     time.sleep(2)
     try:
-        gps_f,gps_l=gps_t.gps_t1()
+        gps_f,gps_l=gps_t.gps_g()
+        print("x:",gps_f,gps_l)
+        sql_01.in_sql(gps_f,gps_l)
+
     except:
         gps_f,gps_l=0,0
     influx_test.sql(temperature, humidity,lightLevel)#,gps_f,gps_l)
+    
